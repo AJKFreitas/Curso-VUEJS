a6b654c9b37b48136a0c0797884b3f4a7f1288f5
Ângelo Antonio de Freitas Santos <ajkfreitas@gmail.com>
2020-01-04 13:15:47 -0300

    metodos de ateque

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -16,9 +16,25 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+        },
+        attack(especial){
+            this.hurt('playerLife',4,10,especial)
+            this.hurt('monsterLife',7,12,especial)
+        },
+        getRandom(min,max){
+            const value = Math.random() * (min - max) + min
+            return Math.round (value)
+        },
+        hurt(prop,min,max,especial){
+            const plus = especial? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+
         }
     },
     watch:{
-
+        hasResult(value){
+            if (value) this.running = false
+        }
     }
 })
