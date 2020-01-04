7853e745f25ed14bf6642dd1736f8e6e31bf166e
Ângelo Antonio de Freitas Santos <ajkfreitas@gmail.com>
2020-01-04 12:41:56 -0300

    Botoes do jogo

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,12 +1,15 @@
 new Vue({
     el:'#app',
     data:{
-        playerLife:100,
+        playerLife:00,
         monsterLife:100,
+        running:false,
 
     },
     computed:{
-
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods:{
 
